44ba8432b777974f68baf558ea7a514ef6108fe3
Sweetlight95 <princessadesua93@gmail.com>
2022-01-25 15:31:41 +0100

    update

--- src/ams/admin/components/auth/AuthContainer.js
+++ src/ams/admin/components/auth/AuthContainer.js
@@ -1,5 +1,6 @@
-import { Box, Text, Badge } from '@chakra-ui/react';
-import { Image } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react';
+import { Image, Text, Badge, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+import { FiEye } from 'react-icons/fi'
 import icon from '../../../../assets/wave.svg'
 import React from 'react';
 
@@ -13,6 +14,15 @@ const AuthContainer = () => {
             <Text color="#595959" fontSize="14" m={0} fontWeight="500">Welcome to the Attendance Management</Text>
             <Text color="#595959" fontSize="14" m="2" fontWeight="500">Create a Password to complete your Registration</Text>
       </Box>
+      <FormControl mt='2rem'>
+      <FormLabel htmlFor='password'>Password</FormLabel>
+        <InputGroup>   
+          <Input _focusVisible={{outline : "#6e7391 auto 1px"}} px={12} id="password" type='password' borderRadius='8px' height='40px' width='100%' border='0.34px solid #cccccc' mt='4px'/>
+          <InputRightElement children={<FiEye width="20px" color="#cccccc" />}  height="50px" mr={10}/>
+          </InputGroup>
+          <Text mt={1} color="#6e7391" fontSize="10px">Upto 8 characters with an Uppercase, symbol and number</Text>
+      </FormControl>
+      <Button bg="black" color="white" border="none" borderRadius="6px" mt="1rem" width="100%" p={6} height="45px">Create Account</Button>
     </Box>  
   );
 };
